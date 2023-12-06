e57d98373f621675485ebc2b3d330dbc49b41415
Zachary Reece <zachdreece@gmail.com>
2023-12-06 12:03:05 +0100

    day 6 part 1

---
+++ 2023/Day_6.js
@@ -0,0 +1,34 @@
+const examplesRaces = `Time:      7  15   30
+Distance:  9  40  200`
+
+const actualRaces = `Time:        41     77     70     96
+Distance:   249   1362   1127   1011`
+
+function parseNumbers(string) {
+    return string.split(": ")[1].trim().split(" ").filter(v => v).map(v => Number(v.trim()))
+}
+
+function determineWaysToWin(string) {
+    const [timeString, distanceString] = string.split("\n")
+    const times = parseNumbers(timeString)
+    const distances = parseNumbers(distanceString)
+
+    let productOfWaysToWin = 1
+    let currentWaysToWin = 0
+
+    for (let i = 0; i < times.length; i++) {
+        const raceTime = times[i]
+        const bestDistance = distances[i]
+
+        for (let j = 1; j < raceTime; j++) {
+            let distanceTraveled = (raceTime - j) * j
+            if (distanceTraveled > bestDistance) currentWaysToWin++
+        }
+
+        productOfWaysToWin *= currentWaysToWin
+        currentWaysToWin = 0
+    }
+    console.log(productOfWaysToWin)
+}
+
+determineWaysToWin(actualRaces)
